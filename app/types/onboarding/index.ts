5952781b66ab5543e5235cc21febe79403652e5d
Azmat Habibullah <azmathabibullah@gmail.com>
2025-03-09 13:50:47 -0700

    initial commit

---
+++ app/types/onboarding/index.ts
@@ -0,0 +1,29 @@
+export interface OnboardingQuestion {
+  id: string;
+  question: string;
+  options: OnboardingOption[];
+  description?: string;
+}
+
+export interface OnboardingOption {
+  id: string;
+  label: string;
+  icon?: string;
+}
+
+export interface OnboardingAnswer {
+  questionId: string;
+  selectedOptionId: string;
+}
+
+export interface OnboardingState {
+  currentQuestionIndex: number;
+  answers: OnboardingAnswer[];
+  isComplete: boolean;
+}
+
+// Props interface for the component
+export interface OnboardingQuestionnaireProps {
+  questions: OnboardingQuestion[];
+  onComplete: (answers: OnboardingAnswer[]) => void;
+} 
