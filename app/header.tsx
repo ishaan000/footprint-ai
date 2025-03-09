7edc3c5f3d8950b2dd2af08bbe2dd237c5ad930a
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-09 12:40:33 -0700

    wip

--- app/header.tsx
+++ app/header.tsx
@@ -1,7 +1,8 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 import { getUserDetails } from '@/app/actions';
 import { stackServerApp } from '@/stack';
-import Link from 'next/link';
-import Image from 'next/image';
 
 export async function Header() {
   const user = await stackServerApp.getUser();
@@ -9,48 +10,46 @@ export async function Header() {
   const userProfile = await getUserDetails(user?.id);
 
   return (
-    <header className="w-full flex justify-between items-center px-6 py-4 z-10">
-      <div className="font-medium text-[15px] tracking-tight">
-        <Link href="/">
-          <a className="text-black dark:text-white">Footprint</a>
-        </Link>
+    <header className='z-10 flex w-full items-center justify-between px-6 py-4'>
+      <div className='text-[15px] font-medium tracking-tight'>
+        <Link href='/'>Footprint</Link>
       </div>
       {user ? (
-        <div className="flex items-center gap-4">
-          <span className='inline-flex h-8 items-end flex-col'>
-          {userProfile?.name && <span className="text-[14px] text-gray-600 dark:text-gray-300">
-            {`Hello, ${userProfile?.name.split(' ')[0]}`}
-          </span>}
-          <Link
-            href={app.signOut}
-            className="bg-gray-50 px-1 underline text-[11px]  hover:no-underline"
-          >
-            Sign Out
-          </Link>
+        <div className='flex items-center gap-4'>
+          <span className='inline-flex h-8 flex-col items-end'>
+            {userProfile?.name && (
+              <span className='text-[14px] text-gray-600 dark:text-gray-300'>
+                {`Hello, ${userProfile?.name.split(' ')[0]}`}
+              </span>
+            )}
+            <Link
+              href={app.signOut}
+              className='bg-gray-50 px-1 text-[11px] underline hover:no-underline'
+            >
+              Sign Out
+            </Link>
           </span>
-          {
-            userProfile?.raw_json.profile_image_url && 
-            <Image 
+          {userProfile?.raw_json.profile_image_url && (
+            <Image
               src={userProfile?.raw_json.profile_image_url}
-              alt="User avatar"
+              alt='User avatar'
               width={32}
               height={32}
-              className="rounded-full"
+              className='rounded-full'
             />
-          }
-            
+          )}
         </div>
       ) : (
-        <div className="flex items-center gap-3">
+        <div className='flex items-center gap-3'>
           <Link
             href={app.signIn}
-            className="inline-flex h-8 items-center justify-center rounded-md px-4 text-[13px] font-medium text-gray-700 transition-all hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-gray-800"
+            className='inline-flex h-8 items-center justify-center rounded-md px-4 text-[13px] font-medium text-gray-700 transition-all hover:bg-gray-50 dark:text-gray-300 dark:hover:bg-gray-800'
           >
             Log In
           </Link>
           <Link
             href={app.signUp}
-            className="inline-flex h-8 items-center justify-center font-medium  text-center rounded-full outline-none   dark:text-black bg-primary-1 hover:bg-[#00e5bf] whitespace-nowrap px-6 text-[13px] transition-colors duration-200"
+            className='bg-primary-1 inline-flex h-8 items-center justify-center rounded-full px-6 text-center text-[13px] font-medium whitespace-nowrap transition-colors duration-200 outline-none hover:bg-[#00e5bf] dark:text-black'
           >
             Sign Up
           </Link>
