54f38c9c63c6efe9e46127a0284e42817f7aa483
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-09 15:11:59 -0700

    Verify sign in on chat page

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -1,10 +1,14 @@
-'use client';
-
+import { stackServerApp } from '@/stack';
 import { Container } from '@mui/material';
 
 import { Chat } from '@/components/Chat';
 
-export default function ChatPage() {
+export default async function ChatPage() {
+  const user = await stackServerApp.getUser();
+  const userId = user?.id ?? '';
+  if (!userId) {
+    throw new Error('User not found');
+  }
   return (
     <Container maxWidth='lg' className='py-8'>
       <Chat />
