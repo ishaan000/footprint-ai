642964f766ff8ec02cb7f398f6e1108967f64746
Tim Borovkov <tim.borovkov@icloud.com>
2025-03-09 15:09:51 -0700

    Show sign up if not logged in

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import { createOrUpdateUser } from '@/db';
 import { stackServerApp } from '@/stack';
+import { SignUp } from '@stackframe/stack';
 
 export default async function Home() {
   const stackUser = await stackServerApp.getUser();
   if (!stackUser) {
-    // TODO: Redirect to login page
+    // User not signed in, show sign up page
+    return (
+      <SignUp
+        fullPage={true}
+        automaticRedirect={true}
+        firstTab='password'
+        extraInfo={
+          <>
+            By signing up, you agree to our <a href='/terms'>Terms</a>
+          </>
+        }
+      />
+    );
   } else {
     await createOrUpdateUser(
       stackUser.id,
